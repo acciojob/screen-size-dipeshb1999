ac72376f3967deb2ed5fdcbd2a867a1c9dc4e7c9
Badgujar Dipesh <117906055+dipeshb1999@users.noreply.github.com>
2023-05-18 11:08:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+    function updateSize() {
+      var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+      
+      var dimensions = document.getElementById("dimensions");
+      dimensions.textContent = "Width: " + width + ", Height: " + height;
+    }
+
+    window.addEventListener("resize", updateSize);
+    updateSize();
